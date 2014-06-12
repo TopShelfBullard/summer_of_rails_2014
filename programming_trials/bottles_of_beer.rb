ee9ffaf03c340296e3617b1642e7e31a59ff5111
Frederic Henry <frederic@Frederic-Henry.local>
2014-06-12 01:07:19 -0500

    Fix output error for 100 Bottles of Beer.

--- programming_trials/bottles_of_beer.rb
+++ programming_trials/bottles_of_beer.rb
@@ -2,8 +2,8 @@
 99.downto(1) do |number|
   plurality = number != 1 ? "s" : ""
   secondary_plurality = number - 1 != 1 ? "s" : ""
-  puts "#{number} bottle#{plurality} on beer on the wall"
+  puts "#{number} bottle#{plurality} of beer on the wall"
   puts "#{number} bottle#{plurality} of beer."
   puts "Take one down, pass it around"
-  puts "#{number - 1} bottle#{secondary_plurality} on beer on the wall"
+  puts "#{number - 1} bottle#{secondary_plurality} of beer on the wall"
 end
